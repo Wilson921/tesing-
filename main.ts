140b2080a693035d6cfffd46b0424ff343ec3fd0
Wilson921 <111488941+Wilson921@users.noreply.github.com>
2022-08-18 18:59:28 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+})
